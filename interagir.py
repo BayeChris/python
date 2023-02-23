64641631c3c64bf711a0477d2037c710cdc36696
Chrisbaye <baye.christ@gmail.com>
2023-02-23 15:09:25 +0000

    nouvelle ajout

---
+++ interagir.py
@@ -0,0 +1,35 @@
+print("********** BIENVENUE DANS NOTRE PROGRAMME **********")
+print("VOULEZ VOUS VOUS ENREGISTRER OU AFFICHER LA LISTE ?")
+print("TAPEZ [oui] POUR VOUS ENREGISTRER")
+print("TAPEZ [non] POUR AFFICHER LA LISTE DES INSCRITS")
+
+user_choice = "oui"
+
+user_choice = input("oui ou non :")
+print("VOTRE CHOIX EST NON :", user_choice,"\n")
+
+
+if user_choice == "oui":
+    nom = input("Entrer votre nom :")
+    print("VOTRE NOM EST : ", nom)
+   
+    prenom = input("Entrer votre prenom :")
+    print("VOTRE PRENOM EST : ", prenom)
+
+    email = input("Entrer votre email :")
+    print("VOTRE EMAIL EST : ", email)
+
+    telephone = input("Entrer votre telephone :")
+    print("VOTRE TELEPHONE EST : ", telephone)
+
+    infos = f"{nom}  {prenom}  {email}  {telephone}"
+    f = open('userlist.txt','a')
+    with open('userlist.txt',"a") as f:
+        f.write("\n"+infos)
+        f.close()
+        print("ENREGISTREMENT SUCCES","\n")
+
+else:
+    print("NOUS VOUS AFFICHONS LA LISTE DES INSCRITS","\n")
+    
+    
